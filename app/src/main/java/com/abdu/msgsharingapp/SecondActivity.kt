200c60fa9087967b7184922890b29efef3331675
Abdurrahman Ibrahem Ghanem <ghanemabdo@gmail.com>
2019-03-07 18:18:44 -0500

    Section Two, Video Three

---
+++ app/src/main/java/com/abdu/msgsharingapp/SecondActivity.kt
@@ -0,0 +1,13 @@
+package com.abdu.msgsharingapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+
+class SecondActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_second)
+    }
+}
